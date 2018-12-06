456b8174e057dd19299637c081d1c983e593fdde
Amanda Milburn <amanda.milburn@nike.com>
2018-12-06 17:29:04 +0100

    pulling in data

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,9 +1,5 @@
 import * as types from './actionTypes';
 
-// function url() {
-//   return 'www.url.com';
-// }
-
 export function updateView(view) {
     console.log(view);
     return {type: types.UPDATE_VIEW, page: view};
@@ -13,23 +9,23 @@ export function updateLoadingState(isLoading) {
     return {type: types.UPDATE_LOADING, isLoading: isLoading}
 }
 
-export function fetchProductInfo() {
-    console.log('this is running');
+export function fetchProductInfo(gender) {
     return dispatch => {
         dispatch({ type: types.UPDATE_LOADING, isLoading: true })
-        return fetch('http://10.16.2.55:8080/store/products/v1?store=NLD-502', {
-            method: 'GET',
-            mode: 'no-cors',
-        })
-        .then(response => {
-            response.json();
-            console.log(response.json());
-            dispatch({ type: types.UPDATE_VIEW, page: 'results' })
-            dispatch({ type: types.UPDATE_LOADING, isLoading: false })
-        })
-        .catch(error => {
-            console.log(error);
-            dispatch({ type: types.UPDATE_LOADING, isLoading: false })
-        });
+        setTimeout(() => {
+            return fetch(`http://10.16.2.55:8080/store/products/v1?store=NLD-502&type=${gender}`)
+                .then(response => {
+                    return response.json();
+                })
+                .then(json => {
+                    console.log(json);
+                    dispatch({ type: types.UPDATE_VIEW, page: 'results' })
+                    dispatch({ type: types.UPDATE_LOADING, isLoading: false })
+                })
+                .catch(error => {
+                    console.log(error);
+                    dispatch({ type: types.UPDATE_LOADING, isLoading: false })
+                });
+        }, 3000);
     }
 }
